861e4ce529443d4162b1cb7b2f302ed82e5e9ce6
Dylan Piera <dylanpiera@Hotmail.nl>
2018-02-03 20:03:26 +0100

    Add project files.

---
+++ CodenameIndigo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodenameIndigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
