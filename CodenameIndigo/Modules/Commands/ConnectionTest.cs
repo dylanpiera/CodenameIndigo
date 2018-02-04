6ff29281eb9eb072fc1c583963951192d0a52f12
SoaringDylan <dylanpiera@Hotmail.nl>
2018-02-04 21:19:06 +0100

    Add Registration Command
    ?register

---
+++ CodenameIndigo/Modules/Commands/ConnectionTest.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CodenameIndigo.Modules
+{
+    public class ConnectionTest : InteractiveBase
+    {
+        [Command("conntest")]
+        public async Task ConnTest()
+        {
+            MySqlConnection conn = GetClosedConnection();
+            try
+            {
+                await conn.OpenAsync();
+                await Context.Channel.SendMessageAsync("Success.");
+            }
+            catch (Exception e)
+            {
+                await Context.Channel.SendMessageAsync(e.ToString().Length > 1999 ? e.ToString().Remove(1999) : e.ToString());
+                await Program.Log(e.ToString(), "Connection Test", Discord.LogSeverity.Error);
+            }
+            finally
+            {
+                await conn.CloseAsync();
+            }
+        }
+        
+        public static MySqlConnection GetClosedConnection()
+        {
+            string connStr = $"Server={Sneaky.DatabaseUrl};Uid={Sneaky.User};Database=bulbaleague;port=3306;Password={Sneaky.Password};SslMode=none";
+            MySqlConnection conn = new MySqlConnection(connStr) { };
+            return conn;
+        }
+    }
+}
