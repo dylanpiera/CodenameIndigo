124a1e328d82d45bf5ec919654564e247567e1a8
SoaringDylan <dylanpiera@Hotmail.nl>
2018-02-04 21:18:54 +0100

    Add EnsureChannelCriterion
    Makes sure that the reply is in the given channel (checks with ID)

---
+++ CodenameIndigo/Modules/Criteria/EnsureChannelCriterion.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Addons.Interactive;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace CodenameIndigo.Modules.Criteria
+{
+    class EnsureChannelCriterion : ICriterion<SocketMessage>
+    {
+        private readonly ulong _id;
+        public EnsureChannelCriterion(ulong id) => _id = id;
+
+        public Task<bool> JudgeAsync(SocketCommandContext sourceContext, SocketMessage parameter)
+        {
+            bool ok = _id == parameter.Channel.Id;
+            return Task.FromResult(ok);
+        }
+    }
+}
