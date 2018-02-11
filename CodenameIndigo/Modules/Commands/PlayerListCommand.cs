f50783a14a7491e4a9fa5e8b4daac2fc58c0aeb2
SoaringDylan <dylanpiera@Hotmail.nl>
2018-02-11 09:53:21 +0100

    Add PlayerList Command
    Also changes RandomizationHelper to be non random upon request
    (should probably rename the file for said reason)

---
+++ CodenameIndigo/Modules/Commands/PlayerListCommand.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Addons.Interactive;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodenameIndigo.Modules.Commands
+{
+    public class PlayerListCommand : InteractiveBase
+    {
+        [Command("playerlist")]
+        public async Task PlayerList(int id = 1)
+        {
+            List<Player> participants = await RandomizationHelper.GenerateRandomBracketsAsync(id, false);
+
+            string participantString = "Discord Username - Showdown Username\n\n";
+            foreach (Player player in participants)
+            {
+                participantString += $"{player.DiscordName} - {player.ShowdownName}\n";
+            }
+
+            await Context.Channel.SendMessageAsync($"**Full Player List:**\n```{participantString}```");
+        }
+    }
+}
