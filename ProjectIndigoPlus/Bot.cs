fa0ee883b4b3125feff04a285061a9c04c829312
SoaringDylan <dylanpiera@Hotmail.nl>
2018-10-23 13:14:42 +0900

    Start project files
    Start of moving the discord bot from the Discord.Net library to DSharpPlus

---
+++ ProjectIndigoPlus/Bot.cs
@@ -0,0 +1,182 @@
+﻿using DiscordBot.Entities;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class Bot : IDisposable
+    {
+        private DiscordClient _client;
+        private InteractivityModule _interactivity;
+        private CommandsNextModule _cnext;
+        private Config _config;
+        private StartTimes _starttimes;
+        private CancellationTokenSource _cts;
+
+        public Bot()
+        {
+            if (!File.Exists("config.json"))
+            {
+                new Config().SaveToFile("config.json");
+                #region !! Report to user that config has not been set yet !! (aesthetics)
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                WriteCenter("▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒", 2);
+                WriteCenter("▒▒▒▒▒▒▄▀▀▓▓▓▀█▒▒▒▒▒▒");
+                WriteCenter("▒▒▒▒▄▀▓▓▄██████▄▒▒▒▒");
+                WriteCenter("▒▒▒▄█▄█▀░░▄░▄░█▀▒▒▒▒");
+                WriteCenter("▒▒▄▀░██▄░░▀░▀░▀▄▒▒▒▒");
+                WriteCenter("▒▒▀▄░░▀░▄█▄▄░░▄█▄▒▒▒");
+                WriteCenter("▒▒▒▒▀█▄▄░░▀▀▀█▀▒▒▒▒▒");
+                WriteCenter("▒▒▒▄▀▓▓▓▀██▀▀█▄▀▀▄▒▒");
+                WriteCenter("▒▒█▓▓▄▀▀▀▄█▄▓▓▀█░█▒▒");
+                WriteCenter("▒▒▀▄█░░░░░█▀▀▄▄▀█▒▒▒");
+                WriteCenter("▒▒▒▄▀▀▄▄▄██▄▄█▀▓▓█▒▒");
+                WriteCenter("▒▒█▀▓█████████▓▓▓█▒▒");
+                WriteCenter("▒▒█▓▓██▀▀▀▒▒▒▀▄▄█▀▒▒");
+                WriteCenter("▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                WriteCenter("WARNING", 3);
+                Console.ResetColor();
+                WriteCenter("Thank you Mario!", 1);
+                WriteCenter("But our config.json is in another castle!");
+                WriteCenter("(Please fill in the config.json that was generated.)", 2);
+                WriteCenter("Press any key to exit..", 1);
+                Console.SetCursorPosition(0, 0);
+                Console.ReadKey();
+                #endregion
+                Environment.Exit(0);
+            }
+            else
+            {
+                #region welcomemessage
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                WriteCenter(@"  ____              _   _               _____           _ _               ____        _   ");
+                WriteCenter(@" |  _ \            | | (_)             |_   _|         | (_)             |  _ \      | |  ");
+                WriteCenter(@" | |_) | ___   ___ | |_ _ _ __   __ _    | |  _ __   __| |_  __ _  ___   | |_) | ___ | |_ ");
+                WriteCenter(@" |  _ < / _ \ / _ \| __| | '_ \ / _` |   | | | '_ \ / _` | |/ _` |/ _ \  |  _ < / _ \| __|");
+                WriteCenter(@" | |_) | (_) | (_) | |_| | | | | (_| |  _| |_| | | | (_| | | (_| | (_) | | |_) | (_) | |_ ");
+                WriteCenter(@" |____/ \___/ \___/ \__|_|_| |_|\__, | |_____|_| |_|\__,_|_|\__, |\___/  |____/ \___/ \__|");
+                WriteCenter(@"                                 __/ |                       __/ |                        ");
+                WriteCenter(@"                                |___/                       |___/                         ");
+                WriteCenter();
+                Console.ResetColor();
+                #endregion
+            }
+            _config = Config.LoadFromFile("config.json");
+            _client = new DiscordClient(new DiscordConfiguration()
+            {
+                AutoReconnect = true,
+                EnableCompression = true,
+                LogLevel = LogLevel.Debug,
+                Token = _config.Token,
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true
+            });
+
+            _interactivity = _client.UseInteractivity(new InteractivityConfiguration()
+            {
+                PaginationBehaviour = TimeoutBehaviour.Delete,
+                PaginationTimeout = TimeSpan.FromSeconds(30),
+                Timeout = TimeSpan.FromMinutes(5)
+            });
+
+            _starttimes = new StartTimes()
+            {
+                BotStart = DateTime.Now,
+                SocketStart = DateTime.MinValue
+            };
+
+            _cts = new CancellationTokenSource();
+
+            DependencyCollection dep = null;
+            using (DependencyCollectionBuilder d = new DependencyCollectionBuilder())
+            {
+                d.AddInstance(new Dependencies()
+                {
+                    Interactivity = _interactivity,
+                    StartTimes = _starttimes,
+                    Cts = _cts
+                });
+                dep = d.Build();
+            }
+
+            _cnext = _client.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                CaseSensitive = false,
+                EnableDefaultHelp = true,
+                EnableMentionPrefix = true,
+                StringPrefix = _config.Prefix,
+                IgnoreExtraArguments = true,
+                Dependencies = dep
+            });
+
+            RegisterCommands(_cnext);
+
+            _client.Ready += OnReadyAsync;
+        }
+
+        private void RegisterCommands(CommandsNextModule cnext)
+        {
+            _cnext.RegisterCommands<Commands.Owner>();
+            _cnext.RegisterCommands<Commands.Interactivity>();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            WriteCenter("Loaded commands:");
+            Console.ResetColor();
+            WriteCenter("-----");
+            foreach (System.Collections.Generic.KeyValuePair<string, Command> command in _cnext.RegisteredCommands)
+            {
+                WriteCenter(_config.Prefix + command.Key);
+            }
+            WriteCenter("-----");
+            WriteCenter();
+            
+        }
+
+        public async Task RunAsync()
+        {
+            await _client.ConnectAsync();
+            await WaitForCancellationAsync();
+        }
+
+        private async Task WaitForCancellationAsync()
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                await Task.Delay(500);
+            }
+        }
+
+        private async Task OnReadyAsync(ReadyEventArgs e)
+        {
+            await Task.Yield();
+            _starttimes.SocketStart = DateTime.Now;
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            _interactivity = null;
+            _cnext = null;
+            _config = null;
+        }
+
+        internal void WriteCenter(string value = "", int skipline = 0)
+        {
+            for (int i = 0; i < skipline; i++)
+            {
+                Console.WriteLine();
+            }
+
+            Console.SetCursorPosition((Console.WindowWidth - value.Length) / 2, Console.CursorTop);
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ ProjectIndigoPlus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var b = new Bot())
+            {
+                b.RunAsync().Wait();
+            }
+        }
+    }
+}
