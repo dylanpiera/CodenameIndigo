b2dbff06a702aca57e53806c07fcf1f2441e2f4e
Dylan Piera <dylan@fampiera.nl>
2019-03-24 13:29:12 +0100

    Battles due command

--- ProjectIndigoPlus/Modules/CommandModule/OperationalCommands/BattleCommand.cs
+++ ProjectIndigoPlus/Modules/CommandModule/OperationalCommands/BattleCommand.cs
@@ -98,6 +98,40 @@ namespace ProjectIndigoPlus.Modules.Commands
             }
         }
 
+        [Command("battles"),
+        Description("Shows battle for user given as argument.")]
+        public async Task ShowAllBattlesCommand(CommandContext context)
+        {
+            try
+            {
+                await context.Message.CreateReactionAsync(DiscordEmoji.FromName(context.Client, ":typing:"));
+            }
+            catch
+            {
+                await context.Channel.TriggerTypingAsync();
+            }
+
+            DiscordEmbed BattleMessage = await BuildBattleMessage(new DiscordEmbedBuilder()
+            {
+                Color = Bot._config.Color
+            });
+            if (string.IsNullOrEmpty(BattleMessage.Description))
+            {
+                await context.Channel.SendMessageAsync("", false, new DiscordEmbedBuilder()
+                {
+                    Title = $"No battles due.",
+                    Color = DiscordColor.Red,
+                    Description = "In the current state of the tournament, there are no due battles."
+                });
+            }
+            else
+            {
+                await context.Channel.SendMessageAsync("", false, BattleMessage);
+            }
+            await context.Message.DeleteOwnReactionAsync(DiscordEmoji.FromName(context.Client, ":typing:"));
+        }
+
+
         private async Task<DiscordEmbed> BuildBattleMessage(DiscordEmbedBuilder builder, ulong uid)
         {
             MySqlConnection conn = DatabaseHelper.GetClosedConnection();
@@ -117,6 +151,39 @@ namespace ProjectIndigoPlus.Modules.Commands
 
                     while (await reader.ReadAsync())
                     {
+                        builder.Description += $"**{reader.GetString("P1name")}** VS **{reader.GetString("P2name")}**\n\n";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Bot.DebugLogger.LogMessage(DSharpPlus.LogLevel.Critical, "Battle Command" + "", e.ToString(), DateTime.Now);
+            }
+            finally
+            {
+                await conn.CloseAsync();
+            }
+
+            return builder.Build();
+        }
+        private async Task<DiscordEmbed> BuildBattleMessage(DiscordEmbedBuilder builder)
+        {
+            MySqlConnection conn = DatabaseHelper.GetClosedConnection();
+
+            int tid = (await conn.GetLatestTourneyAsync()).Tid;
+
+            try
+            {
+                await conn.OpenAsync();
+
+                MySqlCommand cmd = new MySqlCommand(@"SELECT a.`round` as Round, a.`player1` as P1ID, a.`player2` as P2ID, P1.showdownusername as P1name, P2.showdownusername as P2name FROM `battles` a INNER JOIN `members` AS P1 ON ( P1.`uid` = a.`player1` ) INNER JOIN `members` AS P2 ON ( P2.`uid` = a.`player2` ) WHERE a.`tid` = @tid AND a.`winner` = 0 AND a.`round` IN (SELECT MAX(b.`round`) FROM `battles` b WHERE b.`tid` = a.`tid`)" , conn);
+                cmd.Parameters.Add("tid", MySqlDbType.Int32).Value = tid;
+
+                using (MySqlDataReader reader = (MySqlDataReader)await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        builder.Title = $"Battles due for Round {reader.GetInt32("Round")}";
                         builder.Description += $"**{reader.GetString("P1name")}** VS **{reader.GetString("P2name")}**\n\n";
                     }
                 }
