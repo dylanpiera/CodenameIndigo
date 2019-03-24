331792077aa4e38dac6933ab88afbf66e2cc7983
Dylan Piera <dylan@fampiera.nl>
2019-03-24 16:27:20 +0100

    Add CommandArgs

---
+++ ProjectIndigoPlus/Modules/HelperModule/CommandArgs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectIndigoPlus.Modules.HelperModule
+{
+    public static class CommandArgs
+    {
+        public static Dictionary<ArgType, string> ReadArgs(string[] args)
+        {
+            Dictionary<ArgType, string> output = new Dictionary<ArgType, string>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case string s when s.StartsWith("--f"):
+                        if (!string.IsNullOrEmpty(args.ElementAt(i + 1)))
+                        {
+                            i++;
+                        }
+                            output.Add(ArgType.Force, "true");
+                        continue;
+                }
+            }
+            return output;
+        }
+
+        public static Dictionary<string, string> ReadArgsString(string[] args)
+        {
+            return null;
+        }
+    }
+
+    public enum ArgType
+    {
+        Force,
+    }
+}
